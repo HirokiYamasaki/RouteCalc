12742d6a95797c3b64bccf52742f3a1d75b50780
Hiroki Yamasaki <seiha0218@gmail.com>
2019-05-09 13:09:08 +0900

    ファイル名の変更

---
+++ Ssm.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ */
+
+/**
+ * @author h_yamasaki
+ *
+ */
+public class Ssm {
+
+	/**
+	 * @param args
+	 */
+
+		//public void actionPerformed(ActionEvent e){
+		//	System.out.println("aiueo");
+		//	//String X = E_textX.getText();
+
+		//}
+	public void actionPerformed(ActionEvent e){
+		System.out.println("aiueo");
+		//String X = E_textX.getText();
+
+	}
+
+
+
+
+}
